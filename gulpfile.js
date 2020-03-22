03c03a3535b8c0ead55f20dabfd6fc816a8b2972
fexd12 <felipe.demelo@hotmail.com>
2020-03-21 22:23:36 -0300

    adicionado build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
-var babel =require('gulp-babel');
+// Import `src` and `dest` from gulp for use in the task.
+const { src, dest } = require('gulp');
 
-gulp.task('js',function(){
-    return gulp
-    .src('src/**/*/.js')
-    .pipe(print())
-    .pipe(babel({presets:['env']}))
-    .pipe(gulp.dest('build'))
-})
+// Import Gulp plugins.
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+
+// Gulp 4 uses exported objects as its tasks. Here we only have a
+// single export that represents the default gulp task.
+exports.default = function(done) {
+  // This will grab any file within src/components or its
+  // subdirectories, then ...
+  return src('./src/**/*.js')
+    // Stop the process if an error is thrown.
+    .pipe(plumber())
+    // Transpile the JS code using Babel's preset-env.
+    .pipe(babel({
+      presets: [
+        ['@babel/env', {
+        }]
+      ],plugins:[
+          ['@babel/transform-runtime']
+      ]
+    }))
+    // Save each component as a separate file in dist.
+    .pipe(dest('./dist'))
+};
