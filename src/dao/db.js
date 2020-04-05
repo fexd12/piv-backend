fbcbc46508387f80ab2df8381a3dad9252944379
fexd12 <felipe.demelo@hotmail.com>
2020-04-05 02:24:06 -0300

    editado banco de dados

--- src/dao/db.js
+++ src/dao/db.js
@@ -2,23 +2,23 @@ import {Client} from 'pg';
 require('dotenv').config();
 
 function criaClient(){
-    // return new Client({
-    //     user: 'postgres',
-    //     host: 'localhost',
-    //     database: 'pi_v',
-    //     password: '1234',
-    //     port:5432
-    // });
-
     return new Client({
-        user: process.env.USER,
-        host: process.env.HOST,
-        database: process.env.DATABASE,
-        password: process.env.PASS,
-        port:process.env.PORT_HOST,
-        ssl:true,
-        rejectUnauthorized: true
+        user: 'postgres',
+        host: 'localhost',
+        database: 'pi_v',
+        password: '1234',
+        port:5432
     });
+
+    // return new Client({
+    //     user: process.env.USER,
+    //     host: process.env.HOST,
+    //     database: process.env.DATABASE,
+    //     password: process.env.PASS,
+    //     port:process.env.PORT_HOST,
+    //     ssl:true,
+    //     rejectUnauthorized: true
+    // });
 }
 
 export {
